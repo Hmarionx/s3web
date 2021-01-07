f939583167100e11e0b0632ccb77008df57824f0
Hervé MARION <hm@herves-mac-pro.home>
2021-01-07 08:34:40 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# S3web
+Thymeleaf Upload Download
---
+++ src/test/java/fr/pe/polygone/ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.pe.polygone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
